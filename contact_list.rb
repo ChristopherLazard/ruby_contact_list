02afa9497a0314e312880a27e1ea4edfeddc4c9e
Christopher Lazard <christopher.l.lazard@gmail.com>
2017-11-10 12:24:46 -0700

    new gem and contact loop

--- contact_list.rb
+++ contact_list.rb
@@ -7,14 +7,20 @@ puts '1) Show Contacts'
 puts '2) Add Contacts'
 puts '3) Quit'
 
-user_input = gets.to_i
+contacts = []
+user_inpput = gets.to_i
+
 case user_input
 when 1 
-  puts 'show all contacts'
-when2
+  contacts.each do |contact|
+  puts "Contacts: #{contact}"
+  end
+when 2
   puts 'add a new contact'
 when 3
   puts 'thanks for contact listing...'
   exit
 else
-  puts 'Invalid'  
+  puts 'Invalid'
+ 
+end  
